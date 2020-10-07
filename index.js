b8669e8b7bccd24d79102bc56c0a0e956cbfefd4
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-10-07 20:46:42 +0900

    Update

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ function handleClick() {
   if (DeviceOrientationEvent.requestPermission) {
     DeviceOrientationEvent.requestPermission()
       .then((permissionState) => {
-        if (permissionState === "granted") {
-          listenDeviceOrientationEvent();
+        if (permissionState !== "granted") {
+          return;
         }
+        listenDeviceOrientationEvent();
       })
       .catch(console.error);
-  } else {
-    listenDeviceOrientationEvent();
+    return;
   }
+  listenDeviceOrientationEvent();
 }
 
 function listenDeviceOrientationEvent() {
   window.addEventListener("deviceorientation", function (event) {
-    document.getElementById("x").innerHTML = event.alpha;
+    if (event.beta > 70 && event.beta < 110) {
+      return;
+    }
     document.getElementById("y").innerHTML = event.beta; // 70-110くらいを許容する
-    document.getElementById("z").innerHTML = event.gamma;
   });
 }
