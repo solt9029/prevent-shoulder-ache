70d38c228e17772c298d9e60d7ec02f9cb5829be
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-10-07 20:39:56 +0900

    Update

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
-$(function () {
-  $("#button").click(function () {
-    request_permission();
-  });
-});
-
-function request_permission() {
-  if (
-    DeviceMotionEvent &&
-    DeviceMotionEvent.requestPermission &&
-    typeof DeviceMotionEvent.requestPermission === "function"
-  ) {
-    DeviceMotionEvent.requestPermission();
-  }
-  if (
-    DeviceOrientationEvent &&
-    DeviceOrientationEvent.requestPermission &&
-    typeof DeviceOrientationEvent.requestPermission === "function"
-  ) {
-    DeviceOrientationEvent.requestPermission();
+function handleClick() {
+  if (DeviceOrientationEvent.requestPermission) {
+    DeviceOrientationEvent.requestPermission()
+      .then((permissionState) => {
+        if (permissionState === "granted") {
+          listenDeviceOrientationEvent();
+        }
+      })
+      .catch(console.error);
+  } else {
+    listenDeviceOrientationEvent();
   }
 }
 
-window.addEventListener("deviceorientation", function (evt) {
-  evt.preventDefault();
-  let alpha = evt.alpha;
-  let beta = evt.beta;
-  let gamma = evt.gamma;
-
-  // document.write(`${alpha} ${beta} ${gamma}`);
-});
-
-window.addEventListener("devicemotion", function (evt) {
-  evt.preventDefault();
-  var x = event.accelerationIncludingGravity.x;
-  var y = event.accelerationIncludingGravity.y;
-  var z = event.accelerationIncludingGravity.z;
-
-  document.write(`${x} ${y} ${z}`);
-});
+function listenDeviceOrientationEvent() {
+  window.addEventListener("deviceorientation", function (event) {
+    document.getElementById("x").innerHTML = event.alpha;
+    document.getElementById("y").innerHTML = event.beta; // 70-110くらいを許容する
+    document.getElementById("z").innerHTML = event.gamma;
+  });
+}
